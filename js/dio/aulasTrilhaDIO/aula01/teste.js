6e8b810ef4bb273c66961b91963c74591bc597d1
Iasmim Pereira <iaasmimcristinaa@gmail.com>
2022-08-20 16:14:20 -0300

    Inicialização

---
+++ js/dio/aulasTrilhaDIO/aula01/teste.js
@@ -0,0 +1,21 @@
+var ias = 0; 
+var mim = 1;
+/* Comentários 
+// com
+// várias
+// linhas. */
+// CTRL + / + SELEÇÃO DO QUE É DESEJADO = Tudo estará selecionado.
+
+var preco = 2;
+var desconto = 0.2;
+
+const constante = 10;
+
+preco = preco - desconto;
+
+function soma(a, b) {
+    console.log(a+b);
+    return a + b;
+}
+
+soma(10 + 9); // As funções seguem amesma lógica que Java.
